fdc4f02b8e8a5a26aa6bb78f38f76ed2368eb874
Kevin Fang <kfang@commschool.org>
2017-02-04 15:51:17 -0500

    fixed missed day

--- findHW.py
+++ findHW.py
@@ -5,17 +5,29 @@ with open("hw.txt", "r") as f:
     today = datetime.date.today()
     details = re.search(r'(.*?)-(.*?)-(.*)', str(today))
     month = details.group(2)
-    month = re.sub(r"0(.)", r"\1", month);
+    month = int(re.sub(r"0(.)", r"\1", month));
     day = details.group(3)
     day = int(re.sub(r"0(.)", r"\1", day));
-    weekday = int(datetime.datetime.today().weekday())
+    #weekday = int(datetime.datetime.today().weekday())
 
-    if weekday == 5:
-        day += 2;
-    elif weekday == 6:
-        day += 1;
+    #if weekday == 5:
+    #    day += 2;
+    #elif weekday == 6:
+    #    day += 1;
 
-    regexSearch = re.compile(r"(.+%s\/%d.+)" % (month, day))
-    
+    regexSearch = re.compile(r"(.+%d\/%d.+)" % (month, day))
     todayHW = regexSearch.search(buffer);
-    print todayHW.group(1)
+    try:
+        print todayHW.group(1)
+    except:
+        regexSearch = re.compile(r"(.+%d\/%d.+)" % (month, day + 1))
+        todayHW = regexSearch.search(buffer);
+        try:
+            print todayHW.group(1)
+        except:
+            regexSearch = re.compile(r"(.+%d\/%d.+)" % (month, day + 2))
+            todayHW = regexSearch.search(buffer);
+            try:
+                print todayHW.group(1)
+            except:
+                print("Cannot find!\n");
